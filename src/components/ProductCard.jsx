4bddb7a8c995dad16ab35d305676b6760734edd9
vcabolfazl <104507434+vcabolfazl@users.noreply.github.com>
2023-09-22 09:03:46 +0330

    feat => Development of the home page discount banner section

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,15 +9,15 @@ export default function ProductCard({ id, title, img, price, stars }) {
   colors.splice(colors.indexOf(randomColor), 1);
  }, [])
  return (
-  <div className="border-2 rounded-md p-5 shadow-2xl space-y-3">
+  <div className="relative border-2 rounded-md p-5 shadow-2xl space-y-3">
    <div className="flex items-center justify-center h-[63%]" style={{ background: color }}>
     <img src={img} alt="" />
    </div>
    <div className="font-danaB space-y-4">
     <p className='text-NavyBlue'>{title}</p>
     <p className='text-gray-400 text-left'>{price.toLocaleString()} تومان</p>
-    <div className="flex items-center justify-between">
-     <div className="w-fit bg-zinc-700 px-1.5 py-1 rounded-full">
+    <div className="flex flex-wrap items-center justify-between">
+     <div className="bg-zinc-700 px-1.5 py-1 rounded-full">
       <svg className="w-6 h-6">
        <use href='#shopping-cart'></use>
       </svg>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -80,15 +80,55 @@ export default function Home() {
 
         </div>
       </section>
+
+      {/* --> Start New Products section */}
       <section className='my-16'>
         <div className="container">
           <h3 className='mb-3 text-xl font-danaB text-center'>محصولات جدید</h3>
           <hr />
-          <div className="grid grid-cols-4 gap-12 mt-8">
+          <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-12 mt-8">
             {
               products?.slice(0, 8).map(item => <ProductCard key={item.id} {...item} />)
             }
           </div>
+        </div>
+      </section>
+      {/* End New Products section <-- */}
+
+      <section>
+        <div className="container Discount-banner relative flex flex-wrap sm:flex-nowrap justify-between items-center rounded-lg px-12 py-16">
+          <svg className='absolute top-0 left-8'
+            xmlns="http://www.w3.org/2000/svg"
+            width={95}
+            height={297}
+            viewBox="0 0 95 297"
+            fill="none"
+          >
+            <path
+              d="M0 0H95V267C95 283.569 81.5685 297 65 297H30C13.4315 297 0 283.569 0 267V0Z"
+              fill="#FF6F88"
+              fillOpacity={0.25}
+            />
+          </svg>
+          <div className="z-20 text-white">
+            <h3 className='text-2xl font-danaB mb-4'>50 درصد تخفیف ویژه</h3>
+            <p className='mb-10'>یک استایل شیک و متفاوت را با کفش های اسپرت ما تجربه کنید. ارائه انواع کفش ورزشی با مدل های متنوع. خرید با قیمت‌های باورنکردنی فقط تا 24 اردیبهشت ماه. بشتابید و تا فرصت به اتمام نرسیده از این تخفیف استثنایی استفاده کنید.</p>
+            <Link href={"/"} className='font-danaB text-lg py-2.5 px-6 bg-white/30 rounded-lg'>مشاهده تخفیف‌ها</Link>
+          </div>
+          <img className='z-10 left-0 right-0 mx-auto' src="/assets/Images/Offer.png" alt="Offer.png" />
+          <svg className='absolute right-8 bottom-0'
+            xmlns="http://www.w3.org/2000/svg"
+            width={95}
+            height={297}
+            viewBox="0 0 95 297"
+            fill="none"
+          >
+            <path
+              d="M95 297H0L0 30C0 13.4315 13.4315 0 30 0H65C81.5685 0 95 13.4315 95 30L95 297Z"
+              fill="#FF6F88"
+              fillOpacity={0.25}
+            />
+          </svg>
         </div>
       </section>
     </>
